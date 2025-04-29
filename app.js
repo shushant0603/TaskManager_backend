9b078d1687cc29832b203fca4ed0d270dc8e767a
shushant <shushantkumar0625@gmail.com>
2025-04-29 20:15:01 +0530

    change

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ connectDB();
 app.use(express.json());
 app.use(
     cors({
-        origin: 'http://localhost:5173', // Replace with your frontend URL
+        origin: '*', // Replace with your frontend URL
         credentials: true,
     })
 )
